b643342717e4b63d4b4c21e336781f38bc9ac181
armand667 <armand.damiens31@gmail.com>
2022-03-12 12:36:05 +0100

    switch on 2 case one by one. They became green on click and if you push on 'validation' and if your answer is true it's print on the console GG but if it is flase it is print dommage.

--- sdl_memory/main.cpp
+++ sdl_memory/main.cpp
@@ -12,6 +12,7 @@
 
 void SDL_ExitWithError(const char *message);
 void ConfigBouton(int x, int y, int w, int h, SDL_Renderer *rendu);
+int verification(int case1, int case2, int case3, int case4, int verite);
 
 int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
 {
@@ -31,7 +32,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
     }
 
     //Création fênetre
-    fenetre = SDL_CreateWindow("Le jeu de memoire", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, LARGEUR_FENETRE, HAUTEUR_FENETRE, 0);
+    fenetre = SDL_CreateWindow("Le jeu de memoire", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, LARGEUR_FENETRE, 656, 0);
     //le pointeur window prend en valeur SDL_Create Window
     //SDL_CreateWindow prend en valeur ("Le nom", position x pouvant etre un Flag puis y haut à gauche, largeur, hauteur, mode affichage)
     if(fenetre == NULL)
@@ -75,7 +76,8 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
     int boutonbleue = 15;//indique quel bouton est bleue
     srand( time( NULL ) );//importe le module aléatoire
     int aleatoire = rand()%4;
-    int aleatoire2 = 0;
+    int aleatoire2 = 0;//crée un deuxieme nombre aléatoire entre 0 et 2 pour définir la deuxieme case blue a allumer
+
     //en fonction du résultat du nombre aléatoire, une case différente va devenir bleue
     if (aleatoire == 0)//si le modulo du nombre aléatoire généré est 0 alors
     {
@@ -125,6 +127,8 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
     SDL_RenderPresent(rendu);//pour afficher la case bleue
     SDL_Delay(1000);//pendnat une seconde
 
+    int verite = 0;
+
     //re initialise la couleur à gris
     if(SDL_SetRenderDrawColor(rendu, 236, 236, 236, SDL_ALPHA_OPAQUE)!=0)//COUELUR GRIS DES TOUCHES
     {
@@ -151,6 +155,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(((LARGEUR_FENETRE/2)+5), 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);//case 2 redeviens gris
             SDL_RenderPresent(rendu);// on affiche
+            verite = 12;
         }
         else if(aleatoire2 == 1)
         {
@@ -163,6 +168,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(5, ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
             SDL_RenderPresent(rendu);
+            verite = 13;
         }
         else if (aleatoire2 == 2)
         {
@@ -175,6 +181,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(((LARGEUR_FENETRE/2)+5), ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
             SDL_RenderPresent(rendu);
+            verite = 14;
         }
         /*SDL_RenderPresent(rendu);
         SDL_Delay(1000);*/
@@ -199,6 +206,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(5, 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);//case 0 redeviens gris
             SDL_RenderPresent(rendu);// on affiche
+            verite = 21;
         }
         else if(aleatoire2 == 1)
         {
@@ -211,6 +219,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(5, ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
             SDL_RenderPresent(rendu);
+            verite = 23;
         }
         else if (aleatoire2 == 2)
         {
@@ -223,6 +232,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(((LARGEUR_FENETRE/2)+5), ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
             SDL_RenderPresent(rendu);
+            verite = 24;
         }
         /*SDL_RenderPresent(rendu);
         SDL_Delay(1000);*/
@@ -248,6 +258,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(5, 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);//case 0 redeviens gris
             SDL_RenderPresent(rendu);// on affiche
+            verite = 31;
         }
         else if(aleatoire2 == 1)
         {
@@ -260,6 +271,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(((LARGEUR_FENETRE/2)+5), 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
             SDL_RenderPresent(rendu);
+            verite = 32;
         }
         else if (aleatoire2 == 2)
         {
@@ -272,6 +284,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(((LARGEUR_FENETRE/2)+5), ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
             SDL_RenderPresent(rendu);
+            verite = 34;
         }
 
     }
@@ -295,6 +308,7 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(5, 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);//case 0 redeviens gris
             SDL_RenderPresent(rendu);// on affiche
+            verite = 41;
         }
         else if(aleatoire2 == 1)
         {
@@ -307,10 +321,11 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(((LARGEUR_FENETRE/2)+5), 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
             SDL_RenderPresent(rendu);
+            verite = 42;
         }
         else if (aleatoire2 == 2)
         {
-            ConfigBouton(5, ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);//case 1 redeviens grise
+            ConfigBouton(5, ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);//case 1 devient bleu
             SDL_RenderPresent(rendu);
             SDL_Delay(1000);
             if(SDL_SetRenderDrawColor(rendu, 236, 236, 236, SDL_ALPHA_OPAQUE)!=0)//COUELUR GRIS DES TOUCHES
@@ -319,22 +334,64 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
             }
             ConfigBouton(5, ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);//case 1 redeviens grise
             SDL_RenderPresent(rendu);
+            verite = 43;
         }
     }
     //affiche de nouveau les cases grises.
     SDL_RenderPresent(rendu);
 
+    printf("la combinaison est %d \n", verite);
 
+    //AJOUT DE L'IMAGE POUR VALIDER
+
+    SDL_Surface *validationImg = NULL;//pour importer l'image
+    SDL_Texture *validationTex = NULL;//pour crée une txture avec limage
+    validationImg = SDL_LoadBMP("./picture/validation.bmp");//on va chercher limage
+    if (validationImg == NULL)//si limage est bien here alors on quitte pas
+    {
+        SDL_ExitWithError("Echec lors du chargement de l'image");
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(fenetre);
+    }
+
+    validationTex = SDL_CreateTextureFromSurface(rendu, validationImg);//la texture se crée avec l'image et le rendu en parametre
+    SDL_FreeSurface(validationImg);// on libere image
+    if(validationTex == NULL)//verifie si la texture est bienchargée
+    {
+        SDL_ExitWithError("Echec lors du chargement de l'image");
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(fenetre);
+    }
+
+    SDL_Rect image; //déclare un carré pour l'iamge
+
+    if(SDL_QueryTexture(validationTex, NULL, NULL, &image.w, &image.h)!= 0)//charge l'image en mémoire
+    {
+        SDL_ExitWithError("Echec lors du chargement de la texture en mémoire");
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(fenetre);
+    }
+    image.x = 322;//indique l'emplacement a prendre en largeur
+    image.y = 605;//indique l'emplacement a prendre en hauteur
+
+    if(SDL_RenderCopy(rendu, validationTex, NULL, &image)!=0)//crée un copie de la texture
+    {
+        SDL_ExitWithError("Echec lors de l'affichage du rendu.");
+        SDL_DestroyRenderer(rendu);
+        SDL_DestroyWindow(fenetre);
+    }
+
+    SDL_RenderPresent(rendu);//affiche la texture :)
 
     SDL_bool programme_launched = SDL_TRUE;//un booléen en sdl pour avoir une
 
     //coordonées clic souris .
     int CooX = 0;
     int CooY = 0;
-    int case1 = false;
-    int case2 = false;
-    int case3 = false;
-    int case4 = false;
+    int case1 = 0;
+    int case2 = 0;
+    int case3 = 0;
+    int case4 = 0;
 
     while(programme_launched)//boucle infini pour la fenetre ouverte
     {
@@ -347,7 +404,8 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
                 case SDL_MOUSEBUTTONDOWN:
                     CooX = event.button.x;
                     CooY = event.button.y;
-                    if(CooX > 5 && CooX < ((LARGEUR_FENETRE-15)/2) && CooY > 5 && CooY < ((HAUTEUR_FENETRE-15)/2))//case 1
+                    //CASE VERTE LORS D'UN CLIC
+                    if(CooX > 5 && CooX < ((LARGEUR_FENETRE-15)/2) && CooY > 5 && CooY < ((HAUTEUR_FENETRE-15)/2) && case1 == 0)//case 1
                     {
                         //si click sur la cae 1, elle devient verte
                         if(SDL_SetRenderDrawColor(rendu, 70, 250, 10, SDL_ALPHA_OPAQUE)!=0)//COUELUR VERTE
@@ -356,10 +414,10 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
                         }
                         ConfigBouton(5, 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
                         SDL_RenderPresent(rendu);
-                        case1 = true;
+                        case1 = 1;
 
                     }
-                    else if(CooX > ((LARGEUR_FENETRE/2)+5) && CooX < (LARGEUR_FENETRE-5) && CooY > 5 && CooY < ((HAUTEUR_FENETRE-15)/2))
+                    else if(CooX > ((LARGEUR_FENETRE/2)+5) && CooX < (LARGEUR_FENETRE-5) && CooY > 5 && CooY < ((HAUTEUR_FENETRE-15)/2) && case2 == 0)//case 2
                     {
                         if(SDL_SetRenderDrawColor(rendu, 70, 250, 10, SDL_ALPHA_OPAQUE)!=0)//COUELUR VERTE
                         {
@@ -367,9 +425,9 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
                         }
                         ConfigBouton(((LARGEUR_FENETRE/2)+5), 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
                         SDL_RenderPresent(rendu);
-                        case2 = true;
+                        case2 = 1;
                     }
-                    else if(CooX > 5 && CooX < ((LARGEUR_FENETRE-15)/2) && CooY > ((HAUTEUR_FENETRE/2)+5) && CooY < (HAUTEUR_FENETRE-5))
+                    else if(CooX > 5 && CooX < ((LARGEUR_FENETRE-15)/2) && CooY > ((HAUTEUR_FENETRE/2)+5) && CooY < (HAUTEUR_FENETRE-5) && case3 == 0)//case 3
                     {
                         if(SDL_SetRenderDrawColor(rendu, 70, 250, 10, SDL_ALPHA_OPAQUE)!=0)//COUELUR VERTE
                         {
@@ -377,9 +435,9 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
                         }
                         ConfigBouton(5, ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
                         SDL_RenderPresent(rendu);
-                        case3 = true;
+                        case3 = 1;
                     }
-                    else if(CooX > ((LARGEUR_FENETRE/2)+5) && CooX < (LARGEUR_FENETRE-5) && CooY > ((HAUTEUR_FENETRE/2)+5) && CooY < (HAUTEUR_FENETRE-5))
+                    else if(CooX > ((LARGEUR_FENETRE/2)+5) && CooX < (LARGEUR_FENETRE-5) && CooY > ((HAUTEUR_FENETRE/2)+5) && CooY < (HAUTEUR_FENETRE-5) && case4 == 0)//case 4
                     {
                         if(SDL_SetRenderDrawColor(rendu, 70, 250, 10, SDL_ALPHA_OPAQUE)!=0)//COUELUR VERTE
                         {
@@ -387,9 +445,70 @@ int main( int argc, char ** argv)//inclure argc et argv quand on fait du sdl.
                         }
                         ConfigBouton(((LARGEUR_FENETRE/2)+5), ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
                         SDL_RenderPresent(rendu);
-                        case4 = true;
+                        case4 = 1;
                     }
+                    //REMETTRE LES CASES VERTES EN GRIS SI RE CLIC
+                    else if(CooX > 5 && CooX < ((LARGEUR_FENETRE-15)/2) && CooY > 5 && CooY < ((HAUTEUR_FENETRE-15)/2) && case1 == 1)//case 1
+                    {
+                        //si click sur la cae 1, elle devient verte
+                        if(SDL_SetRenderDrawColor(rendu, 236, 236, 236, SDL_ALPHA_OPAQUE)!=0)//COUELUR GRIS DES TOUCHES
+                        {
+                            SDL_ExitWithError("Impossible de changer la couleur du rendu.");
+                        }
+                        ConfigBouton(5, 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
+                        SDL_RenderPresent(rendu);
+                        case1 = 0;
 
+                    }
+                    else if(CooX > ((LARGEUR_FENETRE/2)+5) && CooX < (LARGEUR_FENETRE-5) && CooY > 5 && CooY < ((HAUTEUR_FENETRE-15)/2) && case2 == 1)
+                    {
+                        if(SDL_SetRenderDrawColor(rendu, 236, 236, 236, SDL_ALPHA_OPAQUE)!=0)//COUELUR GRIS DES TOUCHES
+                        {
+                            SDL_ExitWithError("Impossible de changer la couleur du rendu.");
+                        }
+                        ConfigBouton(((LARGEUR_FENETRE/2)+5), 5, ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
+                        SDL_RenderPresent(rendu);
+                        case2 = 0;
+                    }
+                    else if(CooX > 5 && CooX < ((LARGEUR_FENETRE-15)/2) && CooY > ((HAUTEUR_FENETRE/2)+5) && CooY < (HAUTEUR_FENETRE-5) && case3 == 1)
+                    {
+                        if(SDL_SetRenderDrawColor(rendu, 236, 236, 236, SDL_ALPHA_OPAQUE)!=0)//COUELUR GRIS DES TOUCHES
+                        {
+                            SDL_ExitWithError("Impossible de changer la couleur du rendu.");
+                        }
+                        ConfigBouton(5, ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
+                        SDL_RenderPresent(rendu);
+                        case3 = 0;
+                    }
+                    else if(CooX > ((LARGEUR_FENETRE/2)+5) && CooX < (LARGEUR_FENETRE-5) && CooY > ((HAUTEUR_FENETRE/2)+5) && CooY < (HAUTEUR_FENETRE-5) && case4 == 1)
+                    {
+                        if(SDL_SetRenderDrawColor(rendu, 236, 236, 236, SDL_ALPHA_OPAQUE)!=0)//COUELUR GRIS DES TOUCHES
+                        {
+                            SDL_ExitWithError("Impossible de changer la couleur du rendu.");
+                        }
+                        ConfigBouton(((LARGEUR_FENETRE/2)+5), ((HAUTEUR_FENETRE/2)+5), ((LARGEUR_FENETRE-15)/2), ((HAUTEUR_FENETRE-15)/2), rendu);
+                        SDL_RenderPresent(rendu);
+                        case4 = 0;
+                    }
+                    else if(CooX > 322 && CooX < 478 && CooY > 605 && CooY < 656)//si on clic sur validation
+                    {
+                        if(verification(case1, case2, case3, case4, verite) == 1)
+                        {
+                            printf("GG! vous avez reussi, pas comme si c'etait dure :)\n");
+                        }
+                        else
+                        {
+                            printf("Dommage...\n");
+                        }
+
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                case SDL_KEYUP:
+                    if( event.key.keysym.sym == SDLK_ESCAPE )
+                    {
+                        programme_launched = SDL_FALSE;
+                    }
                     break;
 
                 case SDL_QUIT://pour quitter le programme
@@ -428,4 +547,131 @@ void ConfigBouton(int x, int y, int w, int h, SDL_Renderer *rendu)
     {
         SDL_ExitWithError("Impossible dessiner un rectangle plein .");
     }
+}
+int verification(int case1, int case2, int case3, int case4, int verite)
+{
+    int vof = 0;//bool de vrai ou faux
+    int numerateur = 0;// prend les deux case allumé en valeur
+    if(case1 = 1 && case2 == 1)
+    {
+        numerateur = 12;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case1 = 1 && case3 == 1)
+    {
+        numerateur = 13;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case1 = 1 && case4 == 1)
+    {
+        numerateur = 14;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case2 = 1 && case1 == 1)
+    {
+        numerateur = 21;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case2 = 1 && case3 == 1)
+    {
+        numerateur = 23;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case2 = 1 && case4 == 1)
+    {
+        numerateur = 24;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case3 = 1 && case1 == 1)
+    {
+        numerateur = 31;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case3 = 1 && case2 == 1)
+    {
+        numerateur = 32;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case3 = 1 && case4 == 1)
+    {
+        numerateur = 34;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+
+    else if(case4 = 1 && case1 == 1)
+    {
+        numerateur = 41;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case4 = 1 && case2 == 1)
+    {
+        numerateur = 42;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    else if(case4 = 1 && case3 == 1)
+    {
+        numerateur = 43;
+        if(numerateur == verite)
+        {
+            //gg
+            vof = 1;
+            return vof;
+        }
+    }
+    return (vof);
 }
